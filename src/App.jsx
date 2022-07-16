b14e1ae14bb92c367b96d32a4373d5959d286446
oreste-abizera <oresteabizera11@gmail.com>
2022-07-16 22:12:28 +0200

    render markdown file

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState("");
+
+  const fetchFile = (file) => {
+    let request = new Request(file, {
+      headers: new Headers({
+        "Content-Type": "text/plain",
+      }),
+    });
+
+    return fetch(request)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+
+        return res.text().then((text) => {
+          return text;
+        });
+      })
+      .catch((err) => {
+        throw new Error("Failed fetching file: " + err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchFile("README.md").then((text) => {
+      setMarkdown(text);
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="result">
+      <ReactMarkdown>{markdown}</ReactMarkdown>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
